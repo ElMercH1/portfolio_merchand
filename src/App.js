728d4a8426370fe713787d8cee55b740274c2509
Merchand <95261605+ElMercH1@users.noreply.github.com>
2024-02-25 18:36:48 -0500

    save

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Leonardo Gallegos
         </p>
-        <a
+        <p
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Portfolio proximamente
+        </p>
       </header>
     </div>
   );
