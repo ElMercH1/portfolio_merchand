9041c4522df21db3aef07404252d29ce820b990c
Merchand <95261605+ElMercH1@users.noreply.github.com>
2024-02-27 12:41:04 -0500

    save

--- JS/animacion_titulo.js
+++ JS/animacion_titulo.js
@@ -1,14 +1,18 @@
-$(document).ready(function() {
-    // Escuchar el evento de scroll
-    $(window).scroll(function() {
-      // Obtener la cantidad de desplazamiento vertical
-      var scrollTop = $(this).scrollTop();
-      // Si el usuario ha scrollado más de 100 píxeles, agregar la clase 'left', de lo contrario, removerla
-      if (scrollTop > 100) {
-        $('.contenedor_titulo').addClass('left');
-      } else {
-        $('.contenedor_titulo').removeClass('left');
-      }
-    });
-  });
+var contenedor = document.getElementById('contenedor_logo')
+var imagen = document.getElementById('img_logo')
+
+var bgColor = window.getComputedStyle(contenedor).getPropertyValue('background-color');
+
+// Convierte el color a un formato RGBA
+var rgbaColor = bgColor.match(/\d+/g);
+var r = rgbaColor[0];
+var g = rgbaColor[1];
+var b = rgbaColor[2];
+var a = rgbaColor[3] || 255;
+var luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+// Si el fondo es claro, invierte los colores de la imagen
+if (luminance > 0.5) {
+imagen.style.filter = 'invert(100%)';
+}
   
