d8dfa35fc1e1f545415ac1af1e7b34470352a8eb
Merchand <fin.merchand@gmail.com>
2024-03-01 18:46:39 -0500

    save

--- JS/animacion_titulo.js
+++ JS/animacion_titulo.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const logo = document.querySelector('.contenedor_logo img');
-    const video = document.querySelector('.video_intro')
-    const scrollThreshold = 50; // Cambia este valor según la cantidad de scroll deseada
-  
-    window.addEventListener('scroll', function() {
-      if (window.scrollY > scrollThreshold) {
-        video.style.transform = "rotate(-15deg)";
-        logo.style.filter = "invert(100%)";
-      } else {
-        logo.style.filter = "invert(0%)";
-        video.style.transform = "rotate(0deg)";
+  const logo = document.querySelector('.contenedor_logo img');
+  const video = document.querySelector('.video_intro');
+  const scrollThreshold = 50; // Cambia este valor según la cantidad de scroll deseada
+  let isScrolled = false;
 
+  window.addEventListener('scroll', function() {
+      if (!isScrolled && window.scrollY > scrollThreshold) {
+          isScrolled = true;
+          video.style.transform = "rotate(-15deg)";
+          logo.style.filter = "invert(100%)";
+      } else if (isScrolled && window.scrollY <= scrollThreshold) {
+          isScrolled = false;
+          logo.style.filter = "invert(0%)";
+          video.style.transform = "rotate(0deg)";
       }
-    });
+  });
+});
